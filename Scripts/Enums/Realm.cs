a5920e2c159243afe921f64330ce76fa0e174dc9
DWraith23 <bwilson5924@gmail.com>
2025-03-10 14:31:39 -0400

    Changed Realm to class with enum inside.

--- Scripts/Enums/Realm.cs
+++ Scripts/Enums/Realm.cs
@@ -1,15 +1,48 @@
+using System.Collections.Generic;
+
 namespace OvermortalTools.Scripts.Enums;
 
-public enum Realm
+public static class Realm
 {
-        Novice,
-        Connection,
-        Foundation,
-        Virtuoso,
-        NascentSoul,
-        Incarnation,
-        Voidbreak,
-        Wholeness,
-        Perfection,
-        Nirvana,
+        public enum Classification
+        {
+                Novice,
+                Connection,
+                Foundation,
+                Virtuoso,
+                NascentSoul,
+                Incarnation,
+                Voidbreak,
+                Wholeness,
+                Perfection,
+                Nirvana,
+        }
+
+        public static readonly Dictionary<Classification, string> Names = new()
+        {
+                { Classification.Novice, "Novice" },
+                { Classification.Connection, "Connection" },
+                { Classification.Foundation, "Foundation" },
+                { Classification.Virtuoso, "Virtuoso" },
+                { Classification.NascentSoul, "Nascent Soul" },
+                { Classification.Incarnation, "Incarnation" },
+                { Classification.Voidbreak, "Voidbreak" },
+                { Classification.Wholeness, "Wholeness" },
+                { Classification.Perfection, "Perfection" },
+                { Classification.Nirvana, "Nirvana" },
+        };
+
+        public static List<string> NamesList => 
+        [
+                "Novice",
+                "Connection",
+                "Foundation",
+                "Virtuoso",
+                "Nascent Soul",
+                "Incarnation",
+                "Voidbreak",
+                "Wholeness",
+                "Perfection",
+                "Nirvana",
+        ];
 }
