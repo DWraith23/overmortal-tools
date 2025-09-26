5df65c87769fd43c1eab7ae1c7e3be0e9f852d43
DWraith23 <bwilson5924@gmail.com>
2025-09-26 11:48:16 -0400

    Added extension for trimming large numbers to strings.

--- Scripts/Extensions.cs
+++ Scripts/Extensions.cs
@@ -19,4 +19,14 @@ public static class Extensions
 
     public static long RoundDown(this double d) => (long)Math.Floor(d);
     public static long RoundUp(this double d) => (long)Math.Ceiling(d);
+
+    public static string FormatLargeNumber(this long number)
+    {
+        if (number < 10000) return number.ToString("N0");
+        if (number < 1000000) return $"{number / 1000f:N2}K";
+        if (number < 1_000_000_000) return $"{number / 1_000_000f:N2}M";
+        if (number < 1_000_000_000_000L) return $"{number / 1_000_000_000d:N2}B";
+        if (number < 1_000_000_000_000_000L) return $"{number / 1_000_000_000_000d:N2}T";
+        return $"{number / 1_000_000_000_000_000d:N2}Q";
+    }
 }
